0989cde662565fdb432f0e3351237723d26316ba
notapresent <notapresent@gmail.com>
2017-02-28 20:21:37 +0000

    Add models to admin interface

--- webscraper/admin.py
+++ webscraper/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Channel, ChannelExtractor, Entry
+
+admin.site.register(Channel)
+admin.site.register(ChannelExtractor)
+admin.site.register(Entry)
