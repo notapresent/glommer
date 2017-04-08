f7fdb1a951f1307e3ee21c21724875cb0f75472b
notapresent <notapresent@gmail.com>
2017-04-09 00:19:21 +0300

    Even more view testing

--- webscraper/tests/test_views.py
+++ webscraper/tests/test_views.py
@@ -4,3 +4,4 @@ class TestIndex(TestCase):
     def test_call_view_loads(self):
         response = self.client.get('/webscraper/')
         self.assertEqual(response.status_code, 200)
+        self.assertIn('webscraper', str(response.content))
