f32185a490fb365a6087b18a3c42e5dd9acd8316
notapresent <notapresent@gmail.com>
2017-04-08 23:37:29 +0300

    Test index view

---
+++ webscraper/tests/test_views.py
@@ -0,0 +1,6 @@
+from django.test import TestCase
+
+class TestIndex(TestCase):
+    def test_call_view_loads(self):
+        response = self.client.get('/webscraper/')
+        self.assertEqual(response.status_code, 200)
