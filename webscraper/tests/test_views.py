990345e6be7d4275f587a74a9c6a77202969898f
notapresent <notapresent@gmail.com>
2017-04-09 04:46:47 +0300

    test redirect to webscraper/

--- webscraper/tests/test_views.py
+++ webscraper/tests/test_views.py
@@ -7,3 +7,8 @@ class TestIndex(TestCase):
         response = self.client.get('/webscraper/')
         self.assertEqual(response.status_code, 200)
         self.assertIn('webscraper', str(response.content))
+
+    def test_call_root_redirects(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, 'webscraper/')
