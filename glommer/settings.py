3454f2492bb9f459349455f8403afaadb6c6880c
notapresent <notapresent@gmail.com>
2017-04-11 07:46:07 +0300

    Fix raven revision detection

--- glommer/settings.py
+++ glommer/settings.py
@@ -180,9 +180,10 @@ if 'SENTRY_DSN' in os.environ:
     INSTALLED_APPS.append('raven.contrib.django.raven_compat')
 
     RAVEN_CONFIG = {
-        'dsn': os.environ.get('SENTRY_DSN'),
-        # If you are using git, you can also automatically configure the
-        # release based on the git info.
-        'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
+        'dsn': os.environ.get('SENTRY_DSN')
     }
 
+    try:
+        RAVEN_CONFIG ['release'] = raven.fetch_git_sha(BASE_DIR)
+    except raven.exceptions.InvalidGitRepository:
+        pass
